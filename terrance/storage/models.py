05fe86f073494a0c864bc91c9f132f6ca8fce57c
Geoffrey Lehée <hello@socketubs.org>
2014-02-07 22:18:16 +0100

    Added platform and arch fields on Package model and better ValidationError on package size

--- terrance/storage/models.py
+++ terrance/storage/models.py
@@ -8,10 +8,14 @@ from ..accounts.models import Member
 class Package(models.Model):
     member = models.ForeignKey(Member)
     version = models.CharField(max_length=20)
+    platform = models.CharField(max_length=30, blank=True, null=True)
+    arch = models.CharField(max_length=20, blank=True, null=True)
     update = models.DateTimeField(auto_now=True)
     package = models.FileField(upload_to='packages')
 
     def clean(self):
-        if self.package.file.size > 20 * 1024:
-            raise ValidationError('Package size too big.')
+        if self.package.file.size > 20 * 1024 * 1024:
+            raise ValidationError(
+                'Package size too big. Got %s (limit is %s).' % (
+                    int(self.package.file.size / 1024 / 1024), 20))
         super(Package, self).clean()
