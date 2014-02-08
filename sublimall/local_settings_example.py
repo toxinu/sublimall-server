dd337519aed1e9e482d048c58cb558598cd4e9c9
Geoffrey Lehée <hello@socketubs.org>
2014-02-08 21:23:01 +0100

    Added local_settings example

---
+++ sublimall/local_settings_example.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from settings import BASE_DIR
+from settings import INSTALLED_APPS
+from settings import MIDDLEWARE_CLASSES
+from settings import TEMPLATE_CONTEXT_PROCESSORS
