24515e08362cdfc1ee6e4a8582ae4988055ca946
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-02-10 15:42:22 +0100

    Update admin

--- sublimall/accounts/admin.py
+++ sublimall/accounts/admin.py
@@ -1,39 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
 
 from .models import Member
 from .models import Registration
 
 
-class MemberInline(admin.TabularInline):
-    model = Member
-    can_delete = False
-
-
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('get_email', 'api_key', )
-
-    def get_email(self, obj):
-        return obj.user.email
-    get_email.short_description = 'Email'
+    list_display = ('email', 'api_key', 'is_active', )
 
 
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ('get_email', 'key', )
 
     def get_email(self, obj):
-        return obj.member.user.email
+        return obj.member.email
     get_email.short_description = 'Email'
 
 
-class UserAdmin(UserAdmin):
-    inlines = (MemberInline, )
-
-# admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
-# admin.site.register(Member, MemberAdmin)
-
-admin.site.register(Member)
+admin.site.register(Member, MemberAdmin)
 admin.site.register(Registration, RegistrationAdmin)
