cd5059d2890f943ae47b88f99256aeba27caf5a4
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-02-10 17:04:05 +0100

    Added filter in admin

--- sublimall/accounts/admin.py
+++ sublimall/accounts/admin.py
@@ -7,7 +7,7 @@ from .models import Member
 class MemberAdmin(admin.ModelAdmin):
     list_display = (
         'email', 'api_key', 'is_active', 'date_joined', 'last_login', 'is_staff', )
-    list_filter = ('is_staff', 'date_joined', 'last_login', )
+    list_filter = ('is_active', 'is_staff', 'date_joined', 'last_login', )
     search_fields = ('id', 'email', )
 
 admin.site.register(Member, MemberAdmin)
