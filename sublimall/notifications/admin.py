850438912eae73a0470995b8ce3f3704981c9a94
Geoffrey LEHEE <g.lehee@jurismarches.com>
2014-02-11 12:13:55 +0100

    Updated admin for Notification

--- sublimall/notifications/admin.py
+++ sublimall/notifications/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 
 from .models import Notification
 
-admin.site.register(Notification)
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('get_display_title', 'added', 'member', 'level', 'is_draft', )
+    list_filter = ('is_draft', 'level', )
+
+    def get_display_title(self, obj):
+        if len(obj.title) > 30:
+            return obj.title[:-4] + ' ...'
+        return obj.title
+    get_display_title.short_description = 'Title'
+
+admin.site.register(Notification, NotificationAdmin)
