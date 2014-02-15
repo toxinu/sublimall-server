e0256198f1f3906fdd0a0dd82ca467514c2b4b61
Geoffrey Lehée <hello@socketubs.org>
2014-02-15 13:28:29 +0100

    Fix admin lookup

--- sublimall/storage/admin.py
+++ sublimall/storage/admin.py
@@ -8,7 +8,7 @@ class PackageAdmin(admin.ModelAdmin):
     list_display = (
         'member', 'version', 'platform', 'arch', 'get_display_size', 'update', )
     list_filter = ('version', 'platform', 'arch', 'update', )
-    search_fields = ('id', 'member', )
+    search_fields = ('id', 'member__email', )
 
     def get_display_size(self, obj):
         return "%.2f MB" % (obj.size / 1024 / 1024)
