4758b552a9ebe586dfd98b33847b9264cc9901ec
Geoffrey Lehée <hello@socketubs.org>
2014-02-19 22:40:03 +0100

    Clean admin for Members

--- sublimall/accounts/admin.py
+++ sublimall/accounts/admin.py
@@ -2,6 +2,12 @@
 from django.contrib import admin
 
 from .models import Member
+from ..storage.models import Package
+
+
+class PackageInline(admin.TabularInline):
+    model = Package
+    max_num = 1
 
 
 class MemberAdmin(admin.ModelAdmin):
@@ -10,6 +16,9 @@ class MemberAdmin(admin.ModelAdmin):
     list_filter = ('is_active', 'is_staff', 'date_joined', 'last_login', )
     search_fields = ('id', 'email', )
     actions = ['resend_registration']
+    actions_on_bottom = True
+    exclude = ('groups', 'user_permissions', )
+    inlines = [PackageInline, ]
 
     def resend_registration(self, request, queryset):
         for member in queryset.all():
