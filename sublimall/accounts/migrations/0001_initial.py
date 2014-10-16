3879e4f40405d298e12d1a302b8d22fc387928a4
Geoffrey Lehée <hello@socketubs.org>
2014-10-16 05:06:05 +0200

    Change migrations from South to Django 1.7

--- sublimall/accounts/migrations/0001_initial.py
+++ sublimall/accounts/migrations/0001_initial.py
@@ -1,96 +1,38 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import sublimall.accounts.utils
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Member'
-        db.create_table('accounts_member', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('is_superuser', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('email', self.gf('django.db.models.fields.EmailField')(blank=True, unique=True, max_length=75)),
-            ('api_key', self.gf('django.db.models.fields.CharField')(blank=True, null=True, max_length=40)),
-            ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('date_joined', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('registration_key', self.gf('django.db.models.fields.CharField')(blank=True, default='54db03979e858c52dc4e7d84a9aa296f5c38418e', null=True, max_length=40)),
-            ('password_key', self.gf('django.db.models.fields.CharField')(blank=True, null=True, max_length=40)),
-        ))
-        db.send_create_signal('accounts', ['Member'])
+    dependencies = [
+        ('auth', '0001_initial'),
+    ]
 
-        # Adding M2M table for field groups on 'Member'
-        m2m_table_name = db.shorten_name('accounts_member_groups')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('member', models.ForeignKey(orm['accounts.member'], null=False)),
-            ('group', models.ForeignKey(orm['auth.group'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['member_id', 'group_id'])
-
-        # Adding M2M table for field user_permissions on 'Member'
-        m2m_table_name = db.shorten_name('accounts_member_user_permissions')
-        db.create_table(m2m_table_name, (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('member', models.ForeignKey(orm['accounts.member'], null=False)),
-            ('permission', models.ForeignKey(orm['auth.permission'], null=False))
-        ))
-        db.create_unique(m2m_table_name, ['member_id', 'permission_id'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Member'
-        db.delete_table('accounts_member')
-
-        # Removing M2M table for field groups on 'Member'
-        db.delete_table(db.shorten_name('accounts_member_groups'))
-
-        # Removing M2M table for field user_permissions on 'Member'
-        db.delete_table(db.shorten_name('accounts_member_user_permissions'))
-
-
-    models = {
-        'accounts.member': {
-            'Meta': {'object_name': 'Member'},
-            'api_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'null': 'True', 'max_length': '40'}),
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'unique': 'True', 'max_length': '75'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'user_set'", 'symmetrical': 'False', 'to': "orm['auth.Group']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'password_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'null': 'True', 'max_length': '40'}),
-            'registration_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'default': "'524541bfbf5236382e672ae0d7b4999741db73a9'", 'null': 'True', 'max_length': '40'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'user_set'", 'symmetrical': 'False', 'to': "orm['auth.Permission']"})
-        },
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'symmetrical': 'False', 'to': "orm['auth.Permission']"})
-        },
-        'auth.permission': {
-            'Meta': {'object_name': 'Permission', 'unique_together': "(('content_type', 'codename'),)", 'ordering': "('content_type__app_label', 'content_type__model', 'codename')"},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'object_name': 'ContentType', 'unique_together': "(('app_label', 'model'),)", 'ordering': "('name',)", 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['accounts']
+    operations = [
+        migrations.CreateModel(
+            name='Member',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
+                ('is_superuser', models.BooleanField(default=False, verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.')),
+                ('email', models.EmailField(blank=True, max_length=75, unique=True, verbose_name='email address')),
+                ('api_key', models.CharField(blank=True, null=True, max_length=40)),
+                ('is_staff', models.BooleanField(default=False, verbose_name='staff status', help_text='Designates whether the user can log into this admin site.')),
+                ('is_active', models.BooleanField(default=False, verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('registration_key', models.CharField(blank=True, null=True, default=sublimall.accounts.utils.get_hash, max_length=40)),
+                ('password_key', models.CharField(blank=True, null=True, max_length=40)),
+                ('groups', models.ManyToManyField(to='auth.Group', related_name='user_set', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of his/her group.', verbose_name='groups', related_query_name='user')),
+                ('user_permissions', models.ManyToManyField(to='auth.Permission', related_name='user_set', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions', related_query_name='user')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+    ]
